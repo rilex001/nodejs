5c1098442d66834736f76168f7a7b5b874fdaea4
rilex001 <ristic1900@gmail.com>
2020-07-31 16:57:04 +0200

    express

--- Express/app.js
+++ Express/app.js
@@ -1,19 +1,21 @@
-const http = require('http');
+const path = require('path')
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+const adminRoutes = require('./routes/admin.js')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
 app.use((req, res, next) => {
-   console.log('In the middleware') 
-   next()  // Allows the request to continue to the next middleware in line
+    res.status(404).sendFile(path.join(__dirname,  'views', '404.html'))
 })
 
-app.use((req, res, next) => {
-    console.log('In another middleware') 
-    res.send('<h1>Hellow  Express !</h1>')
- })
-
-const server = http.createServer(app) 
-
-server.listen(3000);
+app.listen(3000)
